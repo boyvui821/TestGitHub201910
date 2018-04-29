38f20f5c98b9a4e240e96c4c77bee4b8f26729a6
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-04-30 01:05:51 +0700

    Initial Commit

---
+++ TestGitHub2019/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitHub2019
+//
+//  Created by Nguyen Hieu Trung on 4/30/18.
+//  Copyright © 2018 NHTSOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
